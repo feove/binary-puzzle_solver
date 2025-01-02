e9db6a488c7c1bb6c69f28d287a5c136a6f136f2
feove <feove@gmail.com>
2025-01-02 13:09:55 +0100

    add hiding cursor

--- graphical_interface.py
+++ graphical_interface.py
@@ -1,24 +1,36 @@
 import os
 from time import sleep
+import cursor
 
-press_string = "\n    Press 's' to start or 'q' to quit.\n"
+press_string = "\n    Press 's' to start or 'q' to quit."
+
+
+def print_start():
+
+    print("\n\n        ┏━━━━━━━━━━━━━━┓\n        ┃     START    ┃\n        ┗━━━━━━━━━━━━━━┛\n\n\n")
 
 def intro():
 
     os.system('clear')
     sleep(0.3)
-
+    print_start()
     print(press_string)
     sleep(0.3)
-    loader = "\n    ━"
+    loader = "\n    ═"
+
+    #id = os.fork()
+    cursor.show()
 
     for t in range(len("Press 's' to start or 'q' to quit.")):
-        os.system('clear')
-        print(press_string + loader)
 
-        loader += "━"
+        os.system('clear')
+        print_start()
+        print(press_string + loader,end="\n\n\n\n\n\n")
+
+        loader += "═"
+
         sleep(0.1)
-    sleep(0.4)
+    
  
    
 
