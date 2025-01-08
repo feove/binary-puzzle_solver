7a2d30e85bf41a71923f806eb6e488dae9ea6a0b
feove <feove@gmail.com>
2025-01-08 21:41:15 +0100

    New intro

--- graphical_interface.py
+++ graphical_interface.py
@@ -1,11 +1,10 @@
 import os
 from time import sleep
 
-press_string = "          ▁▁▁               ▁▁▁\n    Press ▎s ▎  to start or ▎q ▎ to quit.\n          ▔▔▔               ▔▔▔"
-
-
+press_string = "          ▁▁▁             ▁▁▁\n    Press ▎s ▎to start or ▎q ▎to quit\n          ▔▔▔             ▔▔▔\n\n\n"
+                                                                            
 def print_start():
-    print("\n\n        ┏━━━━━━━━━━━━━━┓\n        ┃     START    ┃\n        ┗━━━━━━━━━━━━━━┛\n\n\n")
+    print("\n\n        ┏━━━━━━━━━━━━━━┓\n        ┃     START    ┃\n        ┗━━━━━━━━━━━━━━┛\n\n\n\n\n")
 
 def cursor_show():
     print('\033[? 25h', end="")
@@ -22,20 +21,30 @@ def intro():
     print_start()
     sleep(0.3)
     print(press_string)
-    loader = "\n    ═"
+    loader = ""
 
     #id = os.fork()
 
-    for t in range(len("Press ┃s┃ to start or ┃q┃ to quit.")):
+    nb_char = len("Press ┃s┃ to start or ┃q┃ to quit      ")
 
-        os.system('clear')
-        print_start()
-        print(press_string + loader,end="\n\n\n\n\n\n")
-       
-        loader += '═'
+    for t in range(nb_char):
+
+        if t == nb_char-1 or t == 0:
+            loader += '┃'
+
+        else:
+            loader += '='
         
-        sleep(0.2)
-    sleep(1)
+        os.system('clear')
+        print(loader)
+        print_start()
+        print(press_string)
+        print(loader)
+
+        sleep(0.1)
+    
+
+    sleep(0.5)
    
     print('\033[? 25h', end="")
 
